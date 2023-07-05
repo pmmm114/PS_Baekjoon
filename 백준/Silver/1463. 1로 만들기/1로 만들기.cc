d861f95a0cb549d12939f87e2e4622ad4d9663dd
pmmm114 <201645058@itc.ac.kr>
2023-07-05 12:40:07 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[1000001] = {0,};
+
+int main() {
+	
+	int N;
+	cin >> N;
+
+	for (int i = 2; i <= N; i++) {
+		arr[i] = arr[i - 1] + 1;
+		if (i % 2 == 0 && arr[i] > arr[i / 2] + 1) arr[i] = arr[i / 2] + 1;
+		if (i % 3 == 0 && arr[i] > arr[i / 3] + 1) arr[i] = arr[i / 3] + 1;
+	}
+	
+	cout << arr[N] << endl;
+
+	return 0;
+}
