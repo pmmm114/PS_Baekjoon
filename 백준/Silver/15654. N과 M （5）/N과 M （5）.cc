e259db5925b92e7f698d3818f71f6f2f17ac6a79
pmmm114 <201645058@itc.ac.kr>
2024-10-10 13:03:24 +0900

    [Silver III] Title: N과 M (5), Time: 248 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int sequence[9];
+int isused[10001];
+int can_num[10001];
+
+void back_tracking(int k) {
+    if(k == M) {
+        for(int i=0;i<M;i++) {
+            cout << sequence[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i=1;i<=10000;i++) {
+        if(can_num[i] == 0) continue;
+        if(isused[i] == 0) {
+            sequence[k] = i;
+            isused[i] = 1;
+            back_tracking(k+1);
+            isused[i] = 0;
+        }
+    }
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+
+    int num;
+    for(int i=0;i<N;i++) {
+        cin >> num;
+        can_num[num]=1;
+    }
+
+    back_tracking(0);
+    return 0;
+}
