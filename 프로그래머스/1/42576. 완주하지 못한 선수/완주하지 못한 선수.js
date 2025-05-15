7efe5aa30c21a8b9bb985409ba31a7618c3d89c2
LeeKangbok <201645058@itc.ac.kr>
2025-05-15 23:55:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 39.63 ms, Memory: 66.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,16 @@
+function solution(participant, completion) {
+    var answer = '';
+    
+    const map = new Map();
+    for(const name of participant) {
+        map.set(name, (map.get(name) || 0) + 1);
+    }
+    
+    for(const name of completion) {
+        map.set(name, map.get(name) - 1);
+    }
+    
+    for(const [name, count] of map) {
+        if(count >= 1) return name;
+    }
+}
